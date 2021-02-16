d35b40f8d8e3ea53fd2dcd630b9f252215b59db9
Aleksey Merkulov <mnement1988@yandex.ru>
2021-02-16 21:39:54 +0300

    Done

---
+++ ConsoleChat/Server/JSON/AuthReg.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+
+namespace Server.JSON
+{
+    public enum AuthRegTypeOfCommand
+    {
+        Registration, Authorization 
+    }
+
+    public class AuthReg : ISerializable
+    {
+        public AuthRegTypeOfCommand TypeOfCommand { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        
+        public string Serialize()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ ConsoleChat/Server/JSON/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text.Json;
+
+namespace Server.JSON
+{
+    public enum MessageTypeofCommand 
+    {
+        TextMessage, // Отправляем сообщение, Указываем TextMessage, AmountMessages и FileName не указываем
+        FileMessage, // Отправляем файл. Указываем FileName, AmountMessages и TextMessage не указываем
+        GetMessages // Получаем последние сообщеня. Указываем AmountMessages, Sender, Recepient обязательно, остальное не указываем
+    }
+    public class Message : ISerializable
+    {
+
+        public MessageTypeofCommand TypeofCommand { get; set; }
+        public string Sender { get; set; }
+        public string Recepient { get; set; }
+        public DateTime DateTime { get; set; }
+        public string TextMessage { get; set; }
+        public string FileName { get; set; }
+        public int AmountMessages { get; set; }
+        
+        public string Serialize()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+    
+}
