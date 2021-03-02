838aad6086e10831a92d80286552bb5a10a60bdc
Alexey051 <alexeyvodopyanov@yandex.ru>
2021-03-02 21:10:09 +0300

    Добавление api в клиент и сервер

--- ConsoleChat/ConsoleChat/JSON/AuthReg.cs
+++ ConsoleChat/ConsoleChat/JSON/AuthReg.cs
@@ -1,7 +1,6 @@
 ﻿using System.Data.SqlTypes;
 using Newtonsoft.Json;
 using JsonSerializer = System.Text.Json.JsonSerializer;
-using api_database;
 
 namespace Server.JSON
 {
@@ -27,26 +26,10 @@ namespace Server.JSON
         {
             if (TypeOfCommand == AuthRegTypeOfCommand.Authorization)
             {
-                Message = DBconnection.Authorization(Login, Password);
-                if (Message == "Authorization success")
-                {
-                    Success = true;
-                }
-                else if (Message == "Authorization fail")
-                {
-                    Success = false;
-                }
+                
             } else if (TypeOfCommand == AuthRegTypeOfCommand.Registration)
             {
-                Message = DBconnection.Registration(Login, Password);
-                if (Message == "Registration success")
-                {
-                    Success = true;
-                }
-                else if (Message == "Registration fail")
-                {
-                    Success = false;
-                }
+                
             }
         }
         public static bool CanDeserialize(string json)
