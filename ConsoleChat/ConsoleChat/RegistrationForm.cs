77dbba40af8a7b8fa2a511df091742eea8de0725
SergeyOlisov <gexsck@outlook.com>
2021-03-19 20:12:58 +0300

    Update RegistrationForm.cs

--- ConsoleChat/ConsoleChat/RegistrationForm.cs
+++ ConsoleChat/ConsoleChat/RegistrationForm.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Text;
+using System.Text.RegularExpressions;
 using Server.JSON;
 using Terminal.Gui;
 
@@ -92,11 +93,23 @@ namespace ConsoleChat
 		private void Button_OKOnClicked(TextField login, TextField password)
 		{
 			_serverConnection = new ServerConnection();
-			AuthReg authReg = new AuthReg();
-			authReg.Login = login.Text.ToString();
-			authReg.Password = password.Text.ToString();
-			authReg.TypeOfCommand = AuthRegTypeOfCommand.Registration;
-			_serverConnection.SendMessage(authReg.Serialize());
+
+			string s;
+			s = login.Text.ToString();
+			Regex regex = new Regex(@"/[А-Яа-я<>{}()'!@#$%^&*]/");
+			MatchCollection matches = regex.Matches(s);
+			if (matches.Count > 0)
+			{
+				MessageBox.Query("Ошибка", "Найдены запрещенные символы");
+			}
+			else
+			{
+				AuthReg authReg = new AuthReg();
+				authReg.Login = login.Text.ToString();
+				authReg.Password = password.Text.ToString();
+				authReg.TypeOfCommand = AuthRegTypeOfCommand.Registration;
+				_serverConnection.SendMessage(authReg.Serialize());
+			}
 			
 		}
 
